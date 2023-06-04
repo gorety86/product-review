6a1edeb16fa1a1851e16172dbb88408e9a20ef2c
gorety <wagutugorety@gmail.com>
2023-06-04 15:55:29 +0300

    make changes in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,29 @@ Product.delete_all
 Review.delete_all
 
 puts "Seeding users..."
-5.times do
+10.times do
   User.create(
     name: Faker::Name.name
   )
 end
-puts "Done seeding users..."
+puts "Done seeding users"
+
+puts "Seeding products..."
+10.times do
+    Product.create(
+        name: Faker::Name.name,
+        price: Faker::Commerce.price
+    )
+end
+puts "Done seeding products..."
+
+puts "Seeding reviews..."
+10.times do
+  Review.create(
+    star_rating: Faker::Number.between(from: 1, to: 10),
+    comment: Faker::Lorem.sentence,
+    user_id: User.pluck(:id).sample,
+    product_id: Product.pluck(:id).sample
+  )
+end
+puts "Done seeding reviews..."
