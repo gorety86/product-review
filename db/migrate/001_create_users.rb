842a044924805c76edcfef5b9bd25c3435042207
gorety <wagutugorety@gmail.com>
2023-06-04 10:51:50 +0300

    made changes to migrate

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     def change
         create_table :user do |t|
             t.string :name
-            t.timestamp
+            t.timestamp null: false
         end
     end
 end
--- db/migrate/003_create_reviews.rb
+++ db/migrate/003_create_reviews.rb
@@ -1,9 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
     def change
         create_table :reviews do |t|
-            t.string :name
             t.integer :star_rating
             t.string :comment
+            t.integer :user_id
+            t.integer :product_id
+            t.timestamps null: false
         end
 
     end
