4cc3abe7da011253908f4a8945cb3a727601c3eb
gorety <wagutugorety@gmail.com>
2023-06-04 16:54:56 +0300

    changes in product.rb

--- models/product.rb
+++ models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(
+            star_rating: star_rating,
+            comment: comment,
+            user: user,
+            product: self
+        )
+    end
 end
