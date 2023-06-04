111f82367baf0df054256ca34f29e6ddd15b3c7c
gorety <wagutugorety@gmail.com>
2023-06-04 17:55:34 +0300

    make changes in product.rb

--- models/user.rb
+++ models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+        self.products.order('reviews.star_rating DESC').first
+    end
 end
