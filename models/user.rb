d9f5ed99dabf9711791c420b0fa86ca126b79154
gorety <wagutugorety@gmail.com>
2023-06-04 20:39:24 +0300

    make changes in user.rb

--- models/user.rb
+++ models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
         self.products.order('reviews.star_rating DESC').first
     end
 
-    def remove_reviews(prod)
-        self.reviews.where(product_id: prod.id).destroy_all
+    def remove_reviews(product)
+        self.reviews.where(product_id: product.id).destroy_all
     end
 end
