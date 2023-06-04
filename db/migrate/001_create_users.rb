760d98933a43588c2e7d5734855c8c41c5ad1cb8
gorety <wagutugorety@gmail.com>
2023-06-04 10:39:05 +0300

    models

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,5 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
     def change
-        
+        create_table :user do |t|
+            t.string :name
+            t.timestamp
+        end
     end
 end
--- db/migrate/002_create_products.rb
+++ db/migrate/002_create_products.rb
@@ -1,3 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
-   
+   def chanhge
+    create_table :products do |t|
+        t.string :name
+        t.integer :price
+    end
+   end
 end
